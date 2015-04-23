0d98a0e7e5f11ce93cf8db65ca307013740885a8
dsdyer <dsdyer@gmail.com>
2015-04-22 23:58:05 -0500

    Fixed indents

--- sumofprimes.js
+++ sumofprimes.js
@@ -1,27 +1,27 @@
 console.time('function');
 var isPrime = function(a) {
-	if (a===2) {
-		return true;
-	} else {
-			var maxTest = Math.floor(Math.sqrt(a));
-			for (i=3;i<=maxTest;i=i+2) {
-				if (a % i === 0) {
-					return false;
-				}
-			}
-				return true;
-	}
-}
+  if (a===2) {
+    return true;
+  } else {
+      var maxTest = Math.floor(Math.sqrt(a));
+      for (i=3;i<=maxTest;i=i+2) {
+        if (a % i === 0) {
+          return false;
+        }
+      }
+        return true;
+      }
+    }
 
 var sum = 17;
 var check = 11;
 
 while (true) {
   if (isPrime(check)) {
-  	if (check >2000000) {
-  		break;
-  	}
-  	sum+=check;
+    if (check >2000000) {
+      break;
+    }
+    sum+=check;
   }
   check = check + 2;
 }

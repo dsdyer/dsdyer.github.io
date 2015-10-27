24f1a7fa40ca9f9d45a783f73c8d23dcaac3babe
dsdyer <dsdyer@gmail.com>
2015-10-27 07:57:32 -0500

    Update miner.js

--- codewars/miner.js
+++ codewars/miner.js
@@ -1,11 +1,38 @@
+var grid = [[true, true, true], [false, false, true], [true, true, true]];
+
 class MineMap {
   constructor(array, start, end) {
-    console.log(start);
+    this.grid = array;
+    this.start = start;
+    this.end = end;
+  }
+
+  checkSquare(square) {
+  	return this.grid[square.x][square.y] || false;
+  }
+
+  checkLeft(square) {
+  	return this.grid[square.x - 1][square.y] || false;
+  }
+
+  checkRight(square) {
+  	return this.grid[square.x + 1][square.y] || false;
+  }
+
+  checkDown(square) {
+  	return this.grid[square.x][square.y + 1] || false;
+  }
+
+  checkUp(square) {
+		return this.grid[square.x][square.y - 1] || false;
   }
 }
 
-let x = new MineMap(1, 2, 3);
+function solve(map, start, end) {
+	let x = new MineMap(map, start, end);
+	return x;
+}
 
-console.log(x);
 
-console.log('x');
+let y = solve(grid, {x:0, y:0}, {x:2,y:0});
+console.log(y);

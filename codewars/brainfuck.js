fe0656382d2bbe4b3b3927af4745fa202b335d4e
David Dyer <ddyer@DYER2.local>
2015-11-03 14:41:41 -0600

    Brainfuck(ing)

--- codewars/brainfuck.js
+++ codewars/brainfuck.js
@@ -1,16 +1,44 @@
+class Brace {
+  constructor(number, type, mate) {
+    this.id = number;
+    this.type = type;
+
+    if (this.type === 'open') {
+      this.mate = new Brace(this.id, 'close', this);
+    } else this.mate = mate;
+  }
+}
+
 class Interpreter{
   constructor (code, input) {
     this.memory = [];
     this.pointer = 0;
 
-    //this.code = code.split("");
-    //this.input = code.split("");
+    this.code = code.split("");
+    this.input = input.split("");
+    this.parseBraces(code);
+    this.inpterpret();
   }
 
-  get output() { // Last Method
-    var output = 'output!';
-    console.log(output);
-    return output;
+  parseBraces(input) {
+    let ends = [];
+    this.code = this.code.map(function(c, i, a){
+      if (c === '[') {
+        let brace = new Brace(i, 'open');
+        ends.push(brace.mate);
+        return brace;
+      }
+      if (c === ']') {
+        return ends.pop();
+      }
+      return c;
+    });
+    console.log('brace_map: ', this.code);
+  }
+
+  inpterpret() {
+    this.output = this.input;
+    console.log(this.output);
   }
 }
 
@@ -19,4 +47,4 @@ function brainLuck(code, input){
   return x.output;
 }
 
-brainLuck(',+[-.,+]', 'Codewars');
+brainLuck('[,+[-[][][].,+]]', 'Codewars'+String.fromCharCode(255));

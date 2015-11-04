4714aec24aa50abe6cfdcc906b34c1249ab081a1
David Dyer <ddyer@DYER2.local>
2015-11-04 10:52:20 -0600

    Removed switch statement

--- codewars/brainfuck.js
+++ codewars/brainfuck.js
@@ -16,6 +16,31 @@ class Interpreter{
     this.code = code.split("");
     this.input = input.split("");
     this.output = [];
+    this.commands = {
+      ">" : function() {
+              this.pointer++;
+              while (this.pointer >= this.memory.length) {  // If we're at the end of or outside memory,
+                this.memory.push(0);                        // exapand it. Memory is infinite.
+              }
+            },
+      "<" : function() {
+              this.pointer--;
+            },
+      "+" : function() {
+              this.memory[this.pointer] = (++this.memory[this.pointer] % 256); // Overflow at 255
+            },
+      "-" : function() {
+              this.memory[this.pointer] = (this.memory[this.pointer] - 1);
+              if (this.memory[this.pointer] < 0) this.memory[this.pointer] = (this.memory[this.pointer] % 255) + 1;
+            },
+      "." : function() {
+              this.output.push(String.fromCharCode(this.memory[this.pointer]));
+            },
+      "," : function() {
+              let shifted = this.input.shift();
+              this.memory[this.pointer] = shifted.charCodeAt(0); // Store character as Unicode value
+            }
+    }
 
     this.parseBraces();
     this.interpret();
@@ -37,53 +62,22 @@ class Interpreter{
   }
 
   interpret() {
-    for (let inst_pointer = 0; inst_pointer < this.code.length;) { // Moving the pointer manually to
-      switch (this.code[inst_pointer].toString()) {                // Make braces less confusing.
-        case ">":
-          this.pointer++;
-          while (this.pointer >= this.memory.length) {  // If we're at the end of or outside memory,
-            this.memory.push(0);                        // exapand it. Memory is infinite.
-          }
-          inst_pointer++;
-          break;
-        case "<":
-          this.pointer--;
-          inst_pointer++;
-          break;
-        case "+":
-          this.memory[this.pointer] = (++this.memory[this.pointer] % 256); // Overflow at 255
-          inst_pointer++;
-          break;
-        case "-":
-          this.memory[this.pointer] = (this.memory[this.pointer] - 1);
-          if (this.memory[this.pointer] < 0) this.memory[this.pointer] = (this.memory[this.pointer] % 255) + 1;
-          inst_pointer++;
-          break;
-        case ".":
-          this.output.push(String.fromCharCode(this.memory[this.pointer]));
-          inst_pointer++;
-          break;
-        case ",":
-          let shifted = this.input.shift();
-          this.memory[this.pointer] = shifted.charCodeAt(0); // Store character as Unicode value
-          inst_pointer++;
-          break;
-        case "[object Object]":  // This is a Brace
-          if (this.code[inst_pointer].type === 'open') {
+    for (let ipointer = 0; ipointer < this.code.length; ipointer++) {
+      let instruction = this.code[ipointer];
+      if (typeof(instruction) === 'string') {
+        this.commands[instruction].call(this); // Call the command with 'this' set the the Interpreter
+      } else {  // This is a Brace
+          if (instruction.type === 'open') {
             if (this.memory[this.pointer] === 0) {
-              inst_pointer = this.code.indexOf(this.code[inst_pointer].mate) + 1;  // Jump to one instruction after
-              break;                                                               // The matching (close) brace
+              ipointer = this.code.indexOf(instruction.mate);  // Jump to the matching (close) brace
             }
-          } else if (this.code[inst_pointer].type === 'close') {
+          } else if (instruction.type === 'close') {
             if (this.memory[this.pointer] !== 0) {
-              inst_pointer = this.code.indexOf(this.code[inst_pointer].mate) + 1;  // Jump to one instruction after
-              break;                                                               // The matching (open) brace
+              ipointer = this.code.indexOf(instruction.mate);  // Jump to the matching (open) brace
             }
           }
-          inst_pointer++;
-          break;
+        }
       }
-    }
     this.output = this.output.join("");
     return;
   }

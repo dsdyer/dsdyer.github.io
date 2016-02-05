4170914bda089518f4ae727b4d3e2140f6c4b52e
David Dyer <ddyer@Administrators-MacBook-Pro-2.local>
2016-02-04 23:34:52 -0600

    ES6 and betterifying it

--- codewars/sudoku.js
+++ codewars/sudoku.js
@@ -7,61 +7,50 @@ function arrayCleaner(array) {  // Returns a deep copy of a multi-dimensional ar
 }
 
 
-function Puzzle(puzzle) {
-  this.puzzle = arrayCleaner(puzzle);
-  this.rows = {};
-  this.columns = {};
-  this.boxes = {};
-  this.blanks = [];
-  this.pointer = 0;
+class Puzzle {
+  contructor(puzzle) {
+    this.puzzle = arrayCleaner(puzzle);
+    this.rows = {};
+    this.columns = {};
+    this.boxes = {};
+    this.blanks = [];
+    this.pointer = 0;
 
-  _possible = [1,2,3,4,5,6,7,8,9];
+    for (i = 0; i < 9; i++) {
+      this.rows[i] = puzzle[i];
 
-  for (i = 0; i < 9; i++) {
-    this.rows[i] = puzzle[i];
-    for (var j = 0; j < 9; j++) {
-      this.columns[j] = this.columns[j] || [];
-      this.columns[j].push(puzzle[i][j]);
+      for (var j = 0; j < 9; j++) {
+        let square = this.puzzle[i][j];
+        this.columns[j] = this.columns[j] || [];
+        this.columns[j].push(square);
 
-      var boxY = Math.floor(i/3);
-      var boxX = Math.floor(j/3);
-      var box = boxY*3 + boxX;
+        var boxY = Math.floor(i/3);
+        var boxX = Math.floor(j/3);
+        var box  = boxY*3 + boxX;
 
-      this.boxes[box] = this.boxes[box] || [];
-      this.boxes[box].push(puzzle[i][j]);
+        this.boxes[box] = this.boxes[box] || [];
+        this.boxes[box].push(square);
 
-      if (this.puzzle[i][j] === 0) {
-        this.puzzle[i][j] = _possible;
+        if (square === 0) {
+          this.blanks.push([i, j]);
+        }
       };
     };
-  };
+  }
 };
 
-function bruteForce(Puzzle, puzzle) {
-  for (i = 0; i < 9; i++) {
-    for (var j = 0; j < 9; j++) {
-      var square = Puzzle.puzzle[i][j];
-      var row = i;
-      var column = j;
-      var box = Math.floor(i/3) * 3 + Math.floor(j/3);
+function bruteForce(Puzzle) {
+  // Increment the square referenced by Puzzle.pointer
+  // If square > 9, decrement pointer
 
-      if (square instanceof Array) {
-        Puzzle.puzzle[i][j] = square.filter(function(e) {
-          return Puzzle.rows[row].indexOf(e) === -1 &&
-                 Puzzle.columns[column].indexOf(e) === -1 &&
-                 Puzzle.boxes[box].indexOf(e) === -1;
-        });
-      }
-      if (Puzzle.puzzle[i][j].length === 1) {
-        Puzzle.puzzle[i][j] = Puzzle.puzzle[i][j][0];
-      }
-    };
-  };
+  // Check if the Puzzle is valid
+  //   Yes: increment pointer
+  //   No: Goto 43
 };
 
 function sudoku(puzzle) {
   //return the solved puzzle as a 2d array of 9 x 9
   var a = new Puzzle(puzzle);
 
-  solveIt(a, puzzle);
+  bruteForce(a);
 };

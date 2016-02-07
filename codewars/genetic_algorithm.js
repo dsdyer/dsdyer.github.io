95d0312b0954dafe51058dceeca1047aa926706f
David Dyer <ddyer@Administrators-MacBook-Pro-2.local>
2016-02-06 21:36:54 -0600

    Starting genetric algorithm

---
+++ codewars/genetic_algorithm.js
@@ -0,0 +1,24 @@
+  // TODO: Translate this to ES6
+
+
+var GeneticAlgorithm = function () {};
+
+GeneticAlgorithm.prototype.generate = function(length) {
+  // TODO: Implement the generate method
+};
+
+GeneticAlgorithm.prototype.select = function(population, fitnesses) {
+  // TODO: Implement the select method
+};
+
+GeneticAlgorithm.prototype.mutate = function(chromosome, p) {
+  // TODO: Implement the mutate method
+};
+
+GeneticAlgorithm.prototype.crossover = function(chromosome1, chromosome2) {
+  // TODO: Implement the crossover method
+};
+
+GeneticAlgorithm.prototype.run = function(fitness, length, p_c, p_m, iterations) {
+  // TODO: Implement the run method
+};

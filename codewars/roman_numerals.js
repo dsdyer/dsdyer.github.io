0197dafe84e259788c6719303f633e5bc4a66fb4
David Dyer <ddyer@Administrators-MacBook-Pro-2.local>
2016-02-08 16:27:24 -0600

    Roman Numeraling

--- codewars/roman_numerals.js
+++ codewars/roman_numerals.js
@@ -25,6 +25,7 @@ class RomanNumerals {
   };
 
     console.log(roman.join(''));
+    return roman.join('');
   }
 
   static fromRoman(string) {
--- codewars/roman_numerals_test.js
+++ codewars/roman_numerals_test.js
@@ -8,19 +8,44 @@
 describe('Roman Numeral Helper', function() {
 
   it('Should take a number and return the Roman Numeral', function() {
-    let my_numeral = ;
-    let correct = ;
-    let input = ;
+    let input = 4;
+    let my_numeral = RomanNumerals.toRoman(input);
+    let correct = 'IV';
 
-    Test.assertEquals(my_numeral, expected, 'Failed on ' + input + '. Should have been ' + correct + '.')
+    Test.assertEquals(my_numeral, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
+
+    input = 100;
+    my_numeral = RomanNumerals.toRoman(input);
+    correct = 'C';
+
+    Test.assertEquals(my_numeral, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
+    
+    input = 1666;
+    my_numeral = RomanNumerals.toRoman(input);
+    correct = 'MDCLXVI';
+
+    Test.assertEquals(my_numeral, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
+
+    
   });
 
   it('Should take a Roman Numeral and return the number', function() {
-    let my_number = ;
-    let correct = ;
-    let input = ;
+    let input = 'IV';
+    let my_number = RomanNumerals.fromRoman(input);
+    let correct = 4;
 
-    Test.assertEquals(my_number, correct, 'Failed on ' + input + '. Should have been ' + correct + '.')
+    Test.assertEquals(my_number, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
 
+    input = 'C';
+    my_number = RomanNumerals.fromRoman(input);
+    correct = 100;
+
+    Test.assertEquals(my_number, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
+
+    input = 'MDCLXVI';
+    my_number = RomanNumerals.fromRoman(input);
+    correct = 1666;
+
+    Test.assertEquals(my_number, correct, 'Failed on ' + input + '. Should have been ' + correct + '.');
   });
 });

725eebab663b73767730b09542354fa91313898b
David Dyer <ddyer@DYER2.local>
2016-02-09 13:52:14 -0600

    Using === like an adult

--- codewars/genetic_algorithm.js
+++ codewars/genetic_algorithm.js
@@ -1,3 +1,16 @@
+// A chromosome is a string of 0s and 1s representing a solution
+// to a given problem. fitness() is a function that chromosome and
+// returns a number from 0-1 that represents the quality of the
+// solution.
+//
+// If fitness(chromosome) === 1, then chromosome represents the
+// ideal solution to that problem.
+//
+// This class creates 500 random chromosomes, chooses the most
+// fit, and re-combines and mutates them to form a new generation.
+// Returns the best solution after 100 generations, or the first
+// ideal solution found.
+
 class GeneticAlgorithm {
   generate(length = 35) {
     let chromosome = [];
@@ -46,50 +59,37 @@ class GeneticAlgorithm {
     return [ng_1, ng_2];
   };
 
-  run(fitness, length = 35, p_c = 0.6, p_m = 0.002, iterations = 100) {
-    let pop_size = 500;
+  run(fitness, length = 35, p_c = 0.6, p_m = 0.002, generations = 100, pop_size = 500) {
     let population = [];
     let fitnesses = [];
 
-    for (let i = 0; i < pop_size; i++) {
+    for (let i = 0; i < pop_size; i++) {      // Setting up the first generation
       progentior = this.generate(length);
       population.push(progentior);
       fitnesses.push(fitness(progentior.join('')));
     }
 
-    for (let i = 0; i < iterations; i++) {
+    for (let i = 0; i < generations; i++) {
       let next_gen = [];
       let next_fit = [];
-      while (next_gen.length < pop_size) {
+      while (next_gen.length < pop_size) {             // Adds 2 offspring to the next generation
         let chosen = this.select(population, fitnesses);
+        if (Math.random() < p_c) chosen = this.crossover(chosen[0], chosen[1]);
 
-        if (Math.random() < p_c) {
-          chosen = this.crossover(chosen[0], chosen[1]);
-        }
         let offspring_1 = this.mutate(chosen[0], p_m);
-        let fitness_1 = fitness(offspring_1.join(''));
-
-        if (fitness_1 == 1) {
-          return offspring_1.join('');
-        };
+        let fit_1 = fitness(offspring_1.join(''));
+        if (fit_1 === 1) return offspring_1.join('');  // Why iterate on perfection?
 
         let offspring_2 = this.mutate(chosen[1], p_m);
-        let fitness_2 = fitness(offspring_2.join(''));
+        let fit_2 = fitness(offspring_2.join(''));
+        if (fit_2 === 1) return offspring_2.join('');
 
-        if (fitness_2 == 1) {
-          return offspring_2.join('');
-        };
-
-        next_gen.push(offspring_1);
-        next_fit.push(fitness_1);
-
-        next_gen.push(offspring_2);
-        next_fit.push(fitness_2);
+        next_gen.push(offspring_1, offspring_2);
+        next_fit.push(fit_1, fit_2);
       }
       population = next_gen;
       fitnesses = next_fit;
     }
-    return population[fitnesses.indexOf(Math.Max(...fitnesses))];
+    return population[fitnesses.indexOf(Math.Max(...fitnesses))];  // This is an ES6 thing!
   };
 }
-

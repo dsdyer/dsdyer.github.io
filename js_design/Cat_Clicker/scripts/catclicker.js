fd72d4c192546fde5c49b81afaf698cbc32e8a9b
David Dyer <ddyer@DYER2.local>
2016-02-11 12:20:13 -0600

    Cat menu

--- js_design/Cat_Clicker/scripts/catclicker.js
+++ js_design/Cat_Clicker/scripts/catclicker.js
@@ -7,21 +7,33 @@ window.onload = function() {
       return function() {
         cat.count++;
         cat.catcount.textContent = cat.count;
-      }
+      };
+    };
+
+    function attachCatMenu(catlist, i) {
+      return function() {
+        console.log(this);
+
+        catlist.forEach(function(x) {x.html.style.display = "none";});
+
+        catlist[i].html.style.display = "block";
+      };
     };
 
     for (var i = 0, l = names.length; i < l; i++) {
       var newCat = {
         "name" : names[i],
         "count" : 0,
-        "html" : document.body.appendChild(document.createElement('div'))
+        "html" : document.getElementById('catarea').appendChild(document.createElement('div'))
       }
       this.catlist.push(newCat);
 
       newCat.html.className = "cat " + newCat.name;
 
+      if (i !== 0) newCat.html.style.display = "none";
+
       newCat.catbanner = newCat.html.appendChild(document.createElement('h1'));
-      newCat.catbanner.textContent = newCat.name;
+      newCat.catbanner.textContent = newCat.name.replace(newCat.name[0], newCat.name[0].toUpperCase());
 
       newCat.catpic = newCat.html.appendChild(document.createElement('img'));
       newCat.catpic.setAttribute('src', './images/' + newCat.name + '.jpg');
@@ -30,7 +42,14 @@ window.onload = function() {
       newCat.catcount.textContent = newCat.count;
 
       newCat.catpic.addEventListener('click', attachCat(newCat), true);
+
+      newCat.menuitem = document.getElementById('catlist').appendChild(document.createElement('li'));
+      newCat.menuitem.textContent = newCat.name.replace(newCat.name[0], newCat.name[0].toUpperCase());
+
+      newCat.menuitem.addEventListener('click', attachCatMenu(this.catlist, i), true);
+
     }
+
 
   };
 

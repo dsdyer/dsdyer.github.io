7c43ff81c491c2ba5017a6cb98fe2c0a5f0e0017
David Dyer <ddyer@DYER2.local>
2016-02-10 18:10:28 -0600

    This is a neat mystery

--- js_design/catclicker.js
+++ js_design/catclicker.js
@@ -1,7 +1,23 @@
-$(document).ready(function() {
-  var count = 0;
-  $('.cat').on('click', function() {
-    count++;
-    $('.count').html(count);
-  });
-});
+window.onload = function() {
+  var cats = document.getElementsByClassName('cat');
+
+
+  for (var i = 0, l = cats.length; i < l; i++) {
+    var cat = cats[i];
+    cat.count = 0;
+
+    cat.addEventListener('click',
+    function() {
+      this.count++;
+      console.log('cat clicked, click count: ' + cat.count);
+      console.log(this.getElementsByClassName('counter')[0].innerHTML);
+      this.getElementsByClassName('counter')[0].textContent = cat.count;
+    }, false);
+  }
+
+  // cats.forEach(function(cat, i) {
+  //   console.log(Object.keys(cat.item(0)));
+
+
+  // });
+};

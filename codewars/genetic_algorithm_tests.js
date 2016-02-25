0ffd692128837fc12da757f60df8c3d8b995464e
David Dyer <ddyer@Administrators-MacBook-Pro-2.local>
2016-02-25 14:18:18 -0600

    Setting up buddy tables

--- codewars/genetic_algorithm_tests.js
+++
@@ -1,9 +0,0 @@
-Test.describe('GeneticAlgorithm', function() {
-
-  it('Should return the correct chromosome', function() {
-    let paragon = "10011011111001011111111010111101101";
-    let x = fitness(paragon);
-
-    Test.assertEquals(new GeneticAlgorithm().run(x, 35, 0.6, 0.002, 100), paragon, "Incorrect solution for chromosome 10011011111001011111111010111101101");
-  });
-});
--- project euler/README.md
+++
@@ -1,2 +0,0 @@
-# astrolabe
-Project Euler!

f22cea33befd28c5aeda2b7c13422c6dbc73c860
David Dyer <ddyer@DYER2.local>
2016-02-26 17:51:20 -0600

    Experimental TableMaker

--- pages/experimental/script/classFinder.js
+++ pages/experimental/script/classFinder.js
@@ -1,3 +1,10 @@
 window.onload = function() {
   console.log('Gonna find some classes');
+
+
+  var http = new XMLHttpRequest();
+  var url = "get_data.php";
+  var params = "lorem=ipsum&name=binny";
+  http.open("POST", url, true);
+
 }

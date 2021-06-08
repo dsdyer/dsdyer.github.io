a0ba59335a81933e6acf3d673c3f75f5870ef119
David Dyer <dsdyer@gmail.com>
2021-06-08 18:36:01 -0500

    removed a comment

--- girlfriend/dissdata/templates/DemoBox.js
+++ girlfriend/dissdata/templates/DemoBox.js
@@ -10,16 +10,12 @@ const DEMOGRAPHICS = [
 ];
 
 const $makeDemographicLI = demographic => {
-  console.log(`demographic: ${JSON.stringify(demographic, null, 2)}`)
-
   const $li = $(`<li class="demographic ">${demographic[0]}: <strong>${demographic[1]}</strong></li>`);
   return $li
 };
 
 export const $makeDemographicList = demographicCount => {
   const $ul = $('<ul class="demographics"></ul>');
-
-
   demographicCount.forEach(demo=>$ul.append($makeDemographicLI(demo)))
   return $ul
 };
@@ -28,11 +24,7 @@ export function $makeDemoBox (participants) {
   const $box = $(`<div></div>`);
   DEMOGRAPHICS.forEach(demo=> {
     const demographicCount = _.toPairs(_.countBy(participants, demo));
-    // console.log(`demographicCount: ${JSON.stringify(demographicCount, null, 2)}`)
-
     $box.append($makeDemographicList(demographicCount))
-    // console.log(`ctb: ${JSON.stringify(demographicCount, null, 2)}`)  
-    // $ul.append($makeDemographicLI(_.countBy(participants, demo)))
   })
   return $box
 }
